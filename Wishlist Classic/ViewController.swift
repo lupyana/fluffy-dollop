a0c086de562dce60b90b8b871b5d057404fbef7f
Lupyana <lupyanambembati@gmail.com>
2020-01-30 11:20:10 +0300

    Initial Commit

---
+++ Wishlist Classic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wishlist Classic
+//
+//  Created by Lupyana Mbembati on 30/01/2020.
+//  Copyright © 2020 Lupyana Mbembati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
