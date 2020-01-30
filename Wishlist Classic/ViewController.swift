2a9d552914525d0ad85c8ea9ba1ecfdf8a4896d5
Lupyana <lupyanambembati@gmail.com>
2020-01-30 11:44:53 +0300

    Wip: Wishlist welcome screen
    - Added button and name field
    - Added name field validation
    - Perform segue to wishlist vc on button press

--- Wishlist Classic/ViewController.swift
+++ Wishlist Classic/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func continueButtonPressed(_ sender: Any) {
+        if (nameTextField.text! == ""){
+            self.makeAlert(alertTitle: "Error", alertMessage: "Name field can't be empty")
+        }else {
+             performSegue(withIdentifier: "toWishListVc", sender: self)
+        }
+       
+    }
+    
+    func makeAlert(alertTitle: String, alertMessage: String) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+        let okButton  = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            //
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ Wishlist Classic/WishListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WishListViewController.swift
+//  Wishlist Classic
+//
+//  Created by Lupyana Mbembati on 30/01/2020.
+//  Copyright © 2020 Lupyana Mbembati. All rights reserved.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+ 
+
+}
